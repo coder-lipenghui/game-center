0eeca3820dad3445f68aae93b8ff8f441d497663
lipenghui <3464285@qq.com>
2019-05-29 18:08:53 +0800

    区服查询form页

--- backend/views/server/_form.php
+++ backend/views/server/_form.php
@@ -10,6 +10,40 @@ use yii\widgets\ActiveForm;
 
 <div class="tab-servers-form">
 
+    <?php $form = ActiveForm::begin(); ?>
 
+    <?= $form->field($model, 'gameId')->textInput() ?>
+
+    <?= $form->field($model, 'distributions')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'index')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'url')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'netPort')->textInput() ?>
+
+    <?= $form->field($model, 'masterPort')->textInput() ?>
+
+    <?= $form->field($model, 'contentPort')->textInput() ?>
+
+    <?= $form->field($model, 'smallDbPort')->textInput() ?>
+
+    <?= $form->field($model, 'bigDbPort')->textInput() ?>
+
+    <?= $form->field($model, 'mergeId')->textInput() ?>
+
+    <?= $form->field($model, 'openDateTime')->textInput() ?>
+
+    <?= $form->field($model, 'createTime')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
 
 </div>
