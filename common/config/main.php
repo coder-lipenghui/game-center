59436afa2e3894f672af36746016acc064ce6c8d
lipenghui <3464285@qq.com>
2019-09-02 13:58:04 +0800

    增加log数据库记录：应用启动、选服前、选服后、进入游戏、创角、升级等日志信息

--- common/config/main.php
+++ common/config/main.php
@@ -10,12 +10,32 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=127.0.0.1;dbname=qiyou_center',
             'username' => 'root',
-            'password' => '7youNew7DBs',
+            'password'=>'7youNew7DBs',
+            'charset' => 'utf8',
+        ],
+        'db_log'=>[
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=qiyou_log',
+            'username' => 'root',
+            'password'=>'7youNew7DBs',
             'charset' => 'utf8',
         ],
         'cache' => [
 //            'class' => 'yii\caching\FileCache',
             'class'=>'yii\caching\MemCache',
+            'servers' => [
+                [
+                    'host' => 'localhost',
+                    'port' => 11211,
+                    'weight' => 100,
+                ],
+                [
+                    'host' => 'localhost',
+                    'port' => 11211,
+                    'weight' => 50,
+                ],
+            ],
+            'useMemcached' => true ,
         ],
     ],
 
