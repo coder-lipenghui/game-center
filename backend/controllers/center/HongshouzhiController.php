e60ab25884329dbee99f94777b91314203b546d6
lipenghui <3464285@qq.com>
2019-10-29 14:42:46 +0800

    红手指渠道SDK接入完成

--- backend/controllers/center/HongshouzhiController.php
+++ backend/controllers/center/HongshouzhiController.php
@@ -8,7 +8,6 @@ use common\helps\CurlHttpClient;
 
 class HongshouzhiController extends CenterController
 {
-
     protected function loginValidate($request, $distribution)
     {
         $url="http://g.gc.com.cn/sdk/checkUsertoken.php";
@@ -16,7 +15,7 @@ class HongshouzhiController extends CenterController
         $body=[
             'user_token'=>$request['token'],
             'mem_id'=>$request['uid'],
-            'app_id'=>$distribution->appId
+            'app_id'=>$distribution->appID
         ];
         $signStr = "app_id=".$body['app_id']."&mem_id=".$body['mem_id']."&user_token=".$body['user_token']."&app_key=".$appKey;
         $body['sign']=md5($signStr);
@@ -27,7 +26,7 @@ class HongshouzhiController extends CenterController
         $result=$curl->sendPostData($url,$params);
         if ($result)
         {
-            $resultArr=json_encode($result,true);
+            $resultArr=json_decode($result,true);
             if ($resultArr['status']=='1')
             {
                 $player = array(
@@ -50,7 +49,49 @@ class HongshouzhiController extends CenterController
         $this->paymentValidateFailed    = "VALIDATE FAILED";
         $this->paymentSuccess           = "SUCCESS";
 
+        $jsonData=file_get_contents("php://input");
+        \Yii::error("请求参数:".$jsonData,"order");
 
+        if (empty($jsonData))
+        {
+            \Yii::error("请求参数为空","order");
+            return false;
+        }else{
+            $urldata = json_decode($jsonData,true);
+            $order_id = isset($urldata['order_id']) ? $urldata['order_id'] : '';
+            $mem_id = isset($urldata['mem_id']) ? $urldata['mem_id'] : '';
+            $app_id = isset($urldata['app_id']) ? intval($urldata['app_id']) : 0;
+            $money = isset($urldata['money']) ? $urldata['money'] : 0.00;
+            $order_status = isset($urldata['order_status']) ? $urldata['order_status'] : '';
+            $paytime = isset($urldata['paytime']) ? intval($urldata['paytime']) : 0;
+            $attach = isset($urldata['attach']) ? $urldata['attach'] : ''; //CP扩展参数
+            $sign = isset($urldata['sign']) ? $urldata['sign'] : ''; // 签名
+            $money = number_format($money,2);
 
+            //1 校验参数合法性
+            if (empty($urldata) || empty($order_id) || empty($mem_id) || empty($app_id) || empty($money)
+                || empty($order_status) || empty($paytime) || empty($attach) || empty($sign)){
+
+                //CP添加自定义参数合法检测
+                \Yii::error("参数校验失败","order");
+                return false;
+            }
+            $appKey=$distribution->appKey;
+
+            $paramStr = "order_id=".$order_id."&mem_id=".$mem_id."&app_id=".$app_id."&money=".$money."&order_status=".$order_status."&paytime=".$paytime."&attach=".$attach."&app_key=".$appKey;
+            $verifySign = md5($paramStr);
+
+            if (0 == strcasecmp($sign, $verifySign)){
+                return [
+                    'orderId'=>$attach,
+                    'distributionOrderId'=>$order_id,
+                    'payTime'=>$paytime,
+                    'payAmount'=>$urldata['money']*100,
+                ];
+            }else{
+                \Yii::error("sign对比失败","order");
+            }
+        }
+        return false;
     }
 }
