d67184cdc8d6a1bc97add4b9726c2d5b5a75c864
lipenghui <3464285@qq.com>
2020-07-02 21:32:45 +0800

    fix:激活码使用异常后激活码在此使用提示已经使用

--- backend/models/center/ActivateCdkey.php
+++ backend/models/center/ActivateCdkey.php
@@ -70,52 +70,67 @@ class ActivateCdkey extends TabCdkeyRecord
                         }
                         if (!$cdkey->used)
                         {
-                            //激活码状态更改
-                            $cdkey->setAttribute('used',1);
-                            if($cdkey->save())
+                            //记录使用信息
+                            $this->setAttribute('gameId',$game->id);
+                            $this->setAttribute('logTime',time());
+                            $this->varietyId=$cdkey->varietyId;
+                            if ($this->save())
                             {
-                                //记录使用信息
-                                $this->setAttribute('gameId',$game->id);
-                                $this->setAttribute('logTime',time());
-                                $this->varietyId=$cdkey->varietyId;
-                                if ($this->save())
+                                //发货
+                                $server=TabServers::findOne(['id'=>$this->serverId]);
+                                $curl=new CurlHttpClient();
+                                $sign=md5($this->roleId.$this->roleName.$this->cdkey.$variety->items.$variety->name.$game->paymentKey);
+                                $body=[
+                                    'roleId'=>$this->roleId,
+                                    'roleName'=>$this->roleName,
+                                    'cdkey'=>$this->cdkey,
+                                    'variety'=>$variety->name,
+                                    'item'=>$variety->items,
+                                    'port'=>$server->masterPort,
+                                    'sign'=>$sign
+                                ];
+                                $get=[
+                                    'sku'=>$game->sku,
+                                    'serverId'=>$server->index,
+                                    'db'=>1
+                                ];
+                                $json=null;
+                                if (true)//新接口
                                 {
-                                    //发货
-                                    $server=TabServers::findOne(['id'=>$this->serverId]);
-                                    $curl=new CurlHttpClient();
+                                    $url='http://'.$server->url.'/api/cdkey?'.http_build_query($get);
+                                    $json=$curl->sendPostData($url,$body);
+                                }else{
                                     $url='http://'.$server->url.'/app/ckgift.php?';
-                                    $sign=md5($this->roleId.$this->roleName.$this->cdkey.$variety->items.$variety->name.$game->paymentKey);
-                                    $body=[
-                                        'roleId'=>$this->roleId,
-                                        'roleName'=>$this->roleName,
-                                        'cdkey'=>$this->cdkey,
-                                        'variety'=>$variety->name,
-                                        'item'=>$variety->items,
-                                        'sign'=>$sign
-                                    ];
                                     $json=$curl->fetchUrl($url.http_build_query($body));
-                                    if ($curl->getHttpResponseCode()==200)
+                                }
+
+                                if ($curl->getHttpResponseCode()==200)
+                                {
+                                    $result=json_decode($json,true);
+                                    $code=$result['code'];
+                                    if ($code==1)
                                     {
-                                        $result=json_decode($json,true);
-                                        $code=$result['code'];
-                                        if ($code==1)
+                                        //激活码状态更改
+                                        $cdkey->setAttribute('used',1);
+                                        if($cdkey->save())
                                         {
                                             return ['code'=>1,'msg'=>'激活成功,请打开背包查看'];
                                         }else{
-                                            //-1：参数错误 -2：连接失败 -3：数据库选取失败 -4：数据写入失败 -5：sign验证失败
-                                            \Yii::error($body,'order');
-                                            return ['code'=>-10,'msg'=>"激活失败[$code]"];
+                                            return ['code'=>-7,'msg'=>'激活码状态更新失败'];
                                         }
                                     }else{
-                                        \Yii::error($url,'cdkey');
-                                        return ['code'=>-9,'msg'=>'激活出现异常'];
+                                        //-1：参数错误 -2：连接失败 -3：数据库选取失败 -4：数据写入失败 -5：sign验证失败
+                                        \Yii::error($body,'order');
+                                        return ['code'=>-10,'msg'=>"激活失败[$code]"];
                                     }
                                 }else{
-                                    return ['code'=>-8,'msg'=>'激活码激活失败'];
+                                    \Yii::error($url.http_build_query($body),'cdkey');
+                                    return ['code'=>-9,'msg'=>'激活出现异常'];
                                 }
                             }else{
-                                return ['code'=>-7,'msg'=>'激活码状态更新失败'];
+                                return ['code'=>-8,'msg'=>'激活码激活失败'];
                             }
+
                         }else{
                             return ['code'=>-6,'msg'=>'该激活码已使用'];
                         }
