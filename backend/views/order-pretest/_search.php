6c66d53d3c32881aa8bb4fcf9ae73eb7480066e8
lipenghui <3464285@qq.com>
2020-09-17 01:37:28 +0800

    style:删测返利页面调整

--- backend/views/order-pretest/_search.php
+++ backend/views/order-pretest/_search.php
@@ -9,7 +9,6 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="tab-orders-pretest-search">
-
     <?php $form = ActiveForm::begin([
         'action' => ['index'],
         'method' => 'get',
@@ -17,32 +16,53 @@ use yii\widgets\ActiveForm;
             'data-pjax' => 1
         ],
     ]); ?>
+    <div class="row">
+        <div class="col-md-3">
+            <?= $form->field($model, 'distributionId')->label("渠道ID") ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'distributionUserId')->label("渠道账号") ?>
+        </div>
+        <div class="col-md-3">
+            <?php  echo $form->field($model, 'rcvRoleName')->label("角色名") ?>
+        </div>
+        <div class="col-md-3">
+            <?php  echo $form->field($model, 'rcvServerId')->label("区服ID") ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <?php  echo $form->field($model, 'got')->label("领取状态") ?>
+        </div>
+        <div class="col-md-3">
+            <?php  echo $form->field($model, 'rcvRoleId')->label("角色ID") ?>
+        </div>
+        <div class="col-md-3">
+            <?php  echo $form->field($model, 'type')->label("返利类型") ?>
+        </div>
+        <div class="col-md-3">
+        </div>
+    </div>
 
-    <?= $form->field($model, 'id') ?>
+    <?//= $form->field($model, 'id') ?>
+    <?//= $form->field($model, 'total') ?>
+    <?//= $form->field($model, 'rate') ?>
 
-    <?= $form->field($model, 'distributionId') ?>
 
-    <?= $form->field($model, 'distributionUserId') ?>
 
-    <?= $form->field($model, 'total') ?>
 
-    <?= $form->field($model, 'rate') ?>
 
-    <?php // echo $form->field($model, 'type') ?>
 
-    <?php // echo $form->field($model, 'got') ?>
 
-    <?php // echo $form->field($model, 'rcvRoleId') ?>
 
-    <?php // echo $form->field($model, 'rcvRoleName') ?>
 
-    <?php // echo $form->field($model, 'rcvServerId') ?>
+
 
     <?php // echo $form->field($model, 'rcvTime') ?>
 
     <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+        <?= Html::submitButton(Yii::t('app', '查询'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', '重置'), ['class' => 'btn btn-outline-secondary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- backend/views/order-pretest/index.php
+++ backend/views/order-pretest/index.php
@@ -7,39 +7,33 @@ use yii\widgets\Pjax;
 /* @var $searchModel backend\models\TabOrdersPretestSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = Yii::t('app', 'Tab Orders Pretests');
+$this->title = Yii::t('app', '删测返利');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="tab-orders-pretest-index">
     <div class="panel panel-default">
-        <div class="panel-body">
-            <?= Html::a(Yii::t('app', 'Create Tab Orders Pretest'), ['create'], ['class' => 'btn btn-success']) ?>
-        </div>
-    </div>
-    <div class="panel panel-default">
         <div class="panel panel-body">
-                <?php Pjax::begin(); ?>
-                                <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-            
-                            <?= GridView::widget([
+            <?php Pjax::begin(); ?>
+            <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+            <?= GridView::widget([
                 'dataProvider' => $dataProvider,
-                'filterModel' => $searchModel,
-        'columns' => [
-                ['class' => 'yii\grid\SerialColumn'],
+//                'filterModel' => $searchModel,
+                'columns' => [
+//                ['class' => 'yii\grid\SerialColumn'],
 
-                            'id',
-            'distributionId',
-            'distributionUserId',
-            'total',
-            'rate',
-            //'type',
-            //'got',
-            //'rcvRoleId',
-            //'rcvRoleName',
-            //'rcvServerId',
-            //'rcvTime:datetime',
+//                  'id',
+//                  'distributionId',
+                    ['attribute'=>'distributionUserId','label'=>'渠道账号'],
+                    ['attribute'=>'total','label'=>'充值金钻'],
+                    ['attribute'=>'rate','label'=>'返利比例(%)'],
+                    //'type',
+                    ['attribute'=>'got','label'=>'是否领取'],
+                    //'rcvRoleId',
+                    ['attribute'=>'rcvRoleName','label'=>'领取角色'],
+                    ['attribute'=>'rcvServerId','label'=>'领取区服'],
+                    ['attribute'=>'rcvTime','label'=>'领取时间'],
 
-                ['class' => 'yii\grid\ActionColumn'],
+//                    ['class' => 'yii\grid\ActionColumn'],
                 ],
                 ]); ?>
             
